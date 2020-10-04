db2d4030efebc35f5b5299944d3878bbf90b7732
thegamecracks <61257169+thegamecracks@users.noreply.github.com>
2020-10-04 00:58:23 -0400

    Add gateway intents
    - main.py now specifies the intents that the Bot has (nothing new has been specified; privileged intents remain off)
    - Changed a random presence title from "Playing game_battleplayervsAI.py" to "Playing dueturn.py" :wink:

--- main.py
+++ main.py
@@ -21,7 +21,12 @@ def main():
 
     settings.setup()
 
-    client = commands.Bot(command_prefix=settings.get_setting('prefix'))
+    intents = discord.Intents.default()
+
+    client = commands.Bot(
+        command_prefix=settings.get_setting('prefix'),
+        intents=intents
+    )
 
 
     @client.event
