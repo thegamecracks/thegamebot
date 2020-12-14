1a2cecd5dfee1747a06f7e6ab8a6bbb338b9de80
thegamecracks <61257169+thegamecracks@users.noreply.github.com>
2020-12-14 13:28:12 -0500

    Add "wordcount" command + update "analysefrequency" command
    - Adds "wordcount" command which generates a pie chart showing the top 15 words of a given text (should probably also include a text version showing top 100 words)
    - "analysefrequency" command
      - Adds drop shadow to labels
      - Fixes y-axis ticks stepping by decimal values
      - Increases cooldown from 2/40s to 3/120s
      - Increases font size to 18

--- bot/commands/graphing.py
+++ bot/commands/graphing.py
@@ -1,4 +1,5 @@
 import asyncio
+import collections
 import functools
 import io
 import multiprocessing
@@ -10,7 +11,9 @@ import aiohttp
 import discord
 from discord.ext import commands
 import humanize
+import matplotlib.patheffects as path_effects
 import matplotlib.pyplot as plt
+from matplotlib.ticker import MaxNLocator
 import numpy as np
 
 from bot import utils
@@ -32,8 +35,63 @@ class Graphing(commands.Cog):
     FREQUENCY_ANALYSIS_FILESIZE_LIMIT = 100_000
     # Maximum file size allowed for client_frequencyanalysis in number of bytes
 
+    WORD_COUNT_NUM_TO_SHOW = 15
+    # Number of words to be included in the graph; the rest are aggregated
+    # into one entry
+
+    TEXT_SHADOW_ALPHA = 0.6
+
     def __init__(self, bot):
         self.bot = bot
+
+
+
+
+
+    async def get_text(self, ctx, text):
+        """Obtain text from the user either in an attachment or from
+        the text argument.
+
+        Returns:
+            Tuple[bool, str]:
+                The boolean indicates whether it was successful at getting
+                the input or not. If False, the following string should
+                be sent to the user.
+
+        """
+        if ctx.message.attachments:
+            a = ctx.message.attachments[0]
+
+            if a.width is not None:
+                # Image/video file
+                return False, 'Attachment must be a text file.'
+
+            if a.size >= self.FREQUENCY_ANALYSIS_FILESIZE_LIMIT:
+                return (
+                    False, 
+                    'Unfortunately I cannot analyse files '
+                    'over {} in size.'.format(
+                        humanize.naturalsize(
+                            self.FREQUENCY_ANALYSIS_FILESIZE_LIMIT
+                        )
+                    )
+                )
+
+            text = (await a.read()).decode('utf-8')
+            # Raises: discord.HTTPException, discord.Forbidden,
+            # discord.NotFound, UnicodeError
+
+        if not text:
+            return False, 'There is no text to analyse.'
+
+        lowered_text = text.lower()
+        if not any(c in lowered_text for c in string.ascii_lowercase):
+            return False, 'There are no english letters in this text.'
+
+        return True, text
+
+
+
 
 
 ##    def frequency_analysis(self, conn, ctx, text):
@@ -66,14 +124,31 @@ class Graphing(commands.Cog):
         ax.bar(list(string.ascii_lowercase),
                char_count,
                color=letter_colors)
+
         # Remove ticks
         ax.tick_params(axis='x', bottom=False)
+
         # Add labels
-        ax.set_title(
-            f'Frequency Analysis for {ctx.author.display_name}',
-            color='#FF8002')
-        ax.set_xlabel('Letter', fontname='calibri', color=get_bot_color_hex())
-        ax.set_ylabel('Count', fontname='calibri', color=get_bot_color_hex())
+        ax.set_title(f'Frequency Analysis for {ctx.author.display_name}')
+        ax.set_xlabel('Letter')
+        ax.set_ylabel('Count')
+
+        # Force integer ticks
+        ax.yaxis.set_major_locator(MaxNLocator(integer=True))
+
+        # Set fonts
+        for item in ([ax.title, ax.xaxis.label, ax.yaxis.label]
+                     + ax.get_xticklabels() + ax.get_yticklabels()):
+            item.set_family('calibri')
+            item.set_fontsize(18)
+            item.set_color(get_bot_color_hex())
+            # Add shadow
+            item.set_path_effects([
+                path_effects.withSimplePatchShadow(
+                    offset=(1, -1),
+                    alpha=self.TEXT_SHADOW_ALPHA
+                )
+            ])
 
         # Create transparent background
         ax.set_facecolor('#00000000')
@@ -98,39 +173,16 @@ class Graphing(commands.Cog):
     @commands.command(
         name='analysefrequency',
         aliases=('analyzefrequency', 'frequencyanalysis'))
-    @commands.cooldown(2, 40, commands.BucketType.channel)
+    @commands.cooldown(3, 120, commands.BucketType.channel)
     @commands.max_concurrency(3, wait=True)
     async def client_frequencyanalysis(self, ctx, *, text=''):
         """Do a frequency analysis of a given text.
 This only processes letters from the english alphabet.
 
 Text can be provided in the command or as a file uploaded with the message."""
-        if ctx.message.attachments:
-            a = ctx.message.attachments[0]
-
-            if a.width is not None:
-                # Image/video file
-                return await ctx.send('Attachment must be a text file.')
-
-            if a.size >= self.FREQUENCY_ANALYSIS_FILESIZE_LIMIT:
-                return await ctx.send(
-                    'Unfortunately I cannot analyse files '
-                    'over {} in size.'.format(
-                        humanize.naturalsize(
-                            self.FREQUENCY_ANALYSIS_FILESIZE_LIMIT)
-                        )
-                    )
-
-            text = (await a.read()).decode('utf-8')
-            # Raises: discord.HTTPException, discord.Forbidden,
-            # discord.NotFound, UnicodeError
-
-        if not text:
-            return await ctx.send('There is no text to analyse.')
-
-        lowered_text = text.lower()
-        if not any(c in lowered_text for c in string.ascii_lowercase):
-            return await ctx.send('There are no english letters in this text.')
+        success, text = await self.get_text(ctx, text)
+        if not success:
+            return await ctx.send(text)
 
         await ctx.trigger_typing()
 
@@ -159,6 +211,147 @@ Text can be provided in the command or as a file uploaded with the message."""
         f.seek(0)
 
         await ctx.send(file=discord.File(f, 'Frequency Analysis.png'))
+
+
+
+
+
+    def word_count_pie(self, ctx, text):
+        """Count the number of each word and return a pie chart.
+
+        Args:
+            ctx (discord.ext.commands.Context)
+            text (str)
+
+        Returns:
+            BytesIO
+
+        """
+        text = text.lower()
+
+        alphabet = frozenset(string.ascii_lowercase)
+        words = collections.Counter()
+
+        last_i = 0
+        reading_word = False
+        for i, c in enumerate(text):
+            if c in alphabet:
+                reading_word = True
+            else:
+                if reading_word:
+                    words[text[last_i:i]] += 1
+                last_i = i + 1
+                reading_word = False
+        else:
+            if reading_word:
+                words[text[last_i:]] += 1
+
+        if not sum(words.values()):
+            raise ValueError(
+                'text must have some words using the english alphabet')
+
+        top_words = words.most_common(self.WORD_COUNT_NUM_TO_SHOW)
+        max_word_count = top_words[0][1]
+
+        sizes = [count / max_word_count for word, count in top_words]
+        labels = [f'{word.title()} ({count})' for word, count in top_words]
+
+        word_colors = plt.cm.hsv([
+            0.8 * i / len(top_words)
+            for i in range(len(sizes), 0, -1)
+        ])
+
+        fig, ax = plt.subplots()
+
+        # Graph pie
+        total_words = sum(words.values())
+
+##        if len(words) > len(top_words):
+##            # Words were left out; add an "other" size
+##            other_count = total_words - sum(count for word, count in top_words)
+##            sizes.append(other_count / total_words)
+##            labels.append(f'Other ({other_count})')
+##            # Use #808080 (grey)
+##            word_colors = np.append(word_colors, [[.5, .5, .5, 1]], 0)
+
+        patches, texts, autotexts = ax.pie(
+            sizes, labels=labels, colors=word_colors, autopct='%1.2g%%',
+            startangle=0
+        )
+        # NOTE: `patches` are the wedges, `texts` are the labels, and
+        # `autotexts` is the autogenerated labels in the wedges from autopct
+
+        ax.axis('equal')  # keeps the pie's size as a circle
+
+        # Add labels
+        if len(words) <= len(top_words):
+            ax.set_title(
+                f'Top Words ({total_words:,} total)\n'
+                f'for {ctx.author.display_name}\n'
+            )
+        else:
+            # Words were left out
+            ax.set_title(
+                f'Top {self.WORD_COUNT_NUM_TO_SHOW} Words '
+                f'({total_words:,} total)\n'
+                f'for {ctx.author.display_name}\n'
+            )
+        # NOTE: newline is appended at the end just to pad it from the labels
+
+        # Set fonts
+        all_text = texts + autotexts
+        for item in ([ax.title] + all_text):
+            item.set_family('calibri')
+            item.set_color('#FF8002')
+        ax.title.set_fontsize(18)
+        for item in all_text:
+            # Add shadow
+            item.set_path_effects([
+                path_effects.withSimplePatchShadow(
+                    offset=(1, -1),
+                    alpha=self.TEXT_SHADOW_ALPHA
+                )
+            ])
+        for item in texts:
+            item.set_fontsize(16)
+        for item in autotexts:
+            item.set_fontsize(12)
+
+        # Create transparent background
+        ax.set_facecolor('#00000000')
+        fig.patch.set_facecolor('#36393F4D')
+
+        f = io.BytesIO()
+        fig.savefig(f, format='png', bbox_inches='tight', pad_inches=0)
+        # bbox_inches, pad_inches: removes padding around the graph
+
+        return f
+
+
+    @commands.command(name='wordcount')
+    @commands.cooldown(3, 120, commands.BucketType.channel)
+    @commands.max_concurrency(3, wait=True)
+    async def client_wordcount(self, ctx, *, text=''):
+        """Count the occurrences of each word in a given text.
+This only processes letters from the english alphabet.
+
+Text can be provided in the command or as a file uploaded with the message."""
+        success, text = await self.get_text(ctx, text)
+        if not success:
+            return await ctx.send(text)
+
+        await ctx.trigger_typing()
+
+        loop = asyncio.get_running_loop()
+
+        with warnings.catch_warnings():
+            warnings.simplefilter('ignore', UserWarning)
+            f = await loop.run_in_executor(
+                None, self.word_count_pie, ctx, text)
+
+        f.seek(0)
+
+        await ctx.send(file=discord.File(f, 'Word Count Pie Chart.png'))
 
 
 
