816ecd0cfa7c6ad04e5b809e085419ee720a1854
thegamecracks <61257169+thegamecracks@users.noreply.github.com>
2021-02-27 12:36:13 -0500

    Add subprocess webserver
    Hosts the webserver using a subprocess instead of having to manually instantiate a new webserver. The webserver can be enabled using the -W/--webserver argument.

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ import argparse
 import asyncio
 import collections
 import datetime
-import time
 import os
+import sys
+import time
 
 import aiohttp
 import discord
@@ -55,11 +56,14 @@ class Bot(BotDatabaseMixin, commands.Bot):
     """A custom version of Bot that allows case-insensitive references
     to cogs. See "?tag case insensitive cogs" on the discord.py server.
     """
-    def __init__(self, *args, **kwargs):
+    def __init__(self, *args, run_ipc=True, **kwargs):
         super().__init__(super().get_prefix, *args, **kwargs)
         self._BotBase__cogs = commands.core._CaseInsensitiveDict()
 
-        self.ipc = ipc.Server(self, secret_key=os.getenv('PyDiscordBotIPCKey'))
+        ipc = None
+        if run_ipc:
+            ipc = ipc.Server(self, secret_key=os.getenv('PyDiscordBotIPCKey'))
+        self.ipc = ipc
 
     async def on_ipc_ready(self):
         print('IPC is ready')
@@ -93,6 +97,61 @@ async def run_server(bot):
     await self._Server__start(self._server, self.port)
 
 
+async def run_ipc_server(bot):
+    """Start the bot's IPC server.
+
+    This is basically a copy of ipc.Server.start except
+    asynchronous methods are executed using async/await
+    instead of loop.run_until_complete.
+
+    """
+    self = bot.ipc
+
+    self.bot.dispatch("ipc_ready")
+
+    self._server = aiohttp.web.Application()
+    self._server.router.add_route("GET", "/", self.handle_accept)
+
+    if self.do_multicast:
+        self._multicast_server = aiohttp.web.Application()
+        self._multicast_server.router.add_route("GET", "/", self.handle_multicast)
+
+        await self._Server__start(self._multicast_server, self.multicast_port)
+
+    await self._Server__start(self._server, self.port)
+
+
+class IPCClientProcess:
+    """Context manager to spawn a subprocess running
+    the IPC client and webserver."""
+
+    def __init__(self):
+        self.proc = None
+
+    async def __aenter__(self):
+        self.proc = await asyncio.create_subprocess_exec(
+            sys.executable, 'webserver\webserver.py',
+            stdout=asyncio.subprocess.PIPE
+        )
+
+        loop = asyncio.get_running_loop()
+        loop.create_task(self.relay())
+
+        return self
+
+    async def __aexit__(self, exc_type, exc_val, exc_traceback):
+        proc, self.proc = self.proc, None
+        proc.terminate()
+        await proc.wait()
+
+    async def relay(self):
+        proc = self.proc
+        while proc.returncode is None:
+            stdout = await proc.stdout.readline()
+            if stdout:
+                print(stdout.decode(), end='')
+
+
 async def main():
     start_time = time.perf_counter()
 
@@ -101,6 +160,10 @@ async def main():
                         help='Enable privileged members intent.')
     parser.add_argument('-P', '--presences', action='store_true',
                         help='Enable privileged presences intent.')
+    parser.add_argument(
+        '-W', '--webserver', dest='ipc', action='store_true',
+        help='Enable the bot webserver.'
+    )
 
     args = parser.parse_args()
 
@@ -124,7 +187,8 @@ async def main():
 
     bot = Bot(
         help_command=helpcommand.HelpCommand(),
-        intents=intents
+        intents=intents,
+        run_ipc=args.ipc
     )
 
     with utils.update_text('Setting up databases',
@@ -159,6 +223,10 @@ async def main():
         bot.load_extension(name)
     print(f'Loaded all extensions         ')
 
+    if args.ipc:
+        # Start IPC server
+        await run_ipc_server(bot)
+
     # Clean up
     del parser, args, attr, i, name
 
@@ -177,14 +245,15 @@ async def main():
 
     # Start the bot
     print('Starting bot')
-    try:
-        await bot.start(TOKEN)
-    except KeyboardInterrupt:
-        logger.info('KeyboardInterrupt: closing bot')
-    except Exception:
-        logger.exception('Exception raised in bot')
-    finally:
-        await bot.close()
+    async with IPCClientProcess():
+        try:
+            await bot.start(TOKEN)
+        except KeyboardInterrupt:
+            logger.info('KeyboardInterrupt: closing bot')
+        except Exception:
+            logger.exception('Exception raised in bot')
+        finally:
+            await bot.close()
 
 
 if __name__ == '__main__':
