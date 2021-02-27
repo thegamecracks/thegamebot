adb7fc2e58aabdd73450199a5f489a76987f95f7
thegamecracks <61257169+thegamecracks@users.noreply.github.com>
2021-02-27 12:53:00 -0500

    Add more stats and buttons
    The homepage now shows the same stats as the "about" command, and provides two buttons for shutting down and restarting the bot.
    BUG: restarting the bot (through IPC at least) may occasionally lead to the IPC websocket closing and therefore breaking the website. I have no idea why.

--- bot/commands/ipc.py
+++ bot/commands/ipc.py
@@ -12,7 +12,53 @@ class IPC(commands.Cog):
         self.bot = bot
 
     @ipc.server.route()
-    async def get_uptime_date(self, data) -> datetime.datetime:
+    async def do_restart(self, data):
+        """Restart the bot."""
+        print('Initiating restart from IPC')
+        return await self.bot.restart()
+
+    @ipc.server.route()
+    async def do_shutdown(self, data):
+        """Shutdown the bot."""
+        print('Initiating shutdown from IPC')
+        return await self.bot.logout()
+
+    @ipc.server.route()
+    async def get_number_commands(self, data) -> int:
+        """Return the number of commands the bot has."""
+        return len(self.bot.commands)
+
+    @ipc.server.route()
+    async def get_number_commands_processed(self, data) -> int:
+        """Return the number of commands that have been processed."""
+        return sum(self.bot.info_processed_commands.values())
+
+    @ipc.server.route()
+    async def get_number_guilds(self, data) -> int:
+        """Return the number of guilds the bot is in."""
+        return len(self.bot.guilds)
+
+    @ipc.server.route()
+    async def get_number_members(self, data) -> dict:
+        """Return the number of members the bot can see,
+        excluding other bots.
+
+        When members intent is disabled, uses an approximate member count.
+
+        {'member_count': int,
+         'is_approximate': bool}
+
+        """
+        is_approximate = not self.bot.intents.members
+        if is_approximate:
+            member_count = sum(g.member_count for g in self.bot.guilds)
+        else:
+            member_count = sum(not u.bot for u in self.bot.users)
+
+        return {'member_count': member_count, 'is_approximate': is_approximate}
+
+    @ipc.server.route()
+    async def get_uptime_date(self, data) -> str:
         """Get the datetime when the bot last connected."""
         dt = self.bot.uptime_last_connect.astimezone(
             datetime.timezone.utc
@@ -29,13 +75,10 @@ class IPC(commands.Cog):
         )
         return utils.timedelta_string(diff)
 
-
-
-
-
-
-
-
+    @ipc.server.route()
+    async def get_uptime_online(self, data) -> bool:
+        """Check if the bot is online or not."""
+        return self.bot.uptime_is_online
 
 
 def setup(bot):
--- webserver/webserver.py
+++ webserver/webserver.py
@@ -1,6 +1,9 @@
+import asyncio
 import datetime
 import os
+import sys
 
+import discord
 from discord.ext import ipc
 from quart import Quart, render_template, request, session
 
@@ -13,13 +16,63 @@ ipc_client = ipc.Client(
 @app.route('/')
 async def index():
     # Uptime field
-    uptime_date = await ipc_client.request('get_uptime_date')
-    uptime_diff = await ipc_client.request('get_uptime_diff')
+    uptime_online = await ipc_client.request('get_uptime_online')
+    uptime_str = 'Offline'
+    if uptime_online:
+        uptime_date = await ipc_client.request('get_uptime_date')
+        uptime_diff = await ipc_client.request('get_uptime_diff')
+
+        uptime_str = f'{uptime_diff} ({uptime_date})'
+
+    # Version
+    vp = sys.version_info
+    python_version = f'{vp.major}.{vp.minor}.{vp.micro}'
+
+    # Count
+    data = await ipc_client.request('get_number_members')
+    is_approximate = data['is_approximate']
+    member_count = '{}{:,}'.format(
+        '~' if is_approximate else '',
+        data['member_count']
+    )
+
+    guild_count = await ipc_client.request('get_number_guilds')
+    guild_count = f'{guild_count:,}'
 
     return await render_template(
         'index.html',
-        uptime=f'{uptime_diff} ({uptime_date})'
+        commands=await ipc_client.request('get_number_commands'),
+        commands_processed=await ipc_client.request(
+            'get_number_commands_processed'),
+        guilds=guild_count,
+        members=member_count,
+        python_version=python_version,
+        dpy_version=discord.__version__,
+        uptime=uptime_str
     )
+
+
+@app.route('/shutdown', methods=['POST'])
+async def shutdown():
+    mode = coro = None
+    form = await request.form
+    if form['turn_off'] == 'Shutdown':
+        mode = 'shutdown'
+        coro = ipc_client.request('do_shutdown')
+    elif form['turn_off'] == 'Restart':
+        mode = 'restart'
+        coro = ipc_client.request('do_restart')
+
+    rendered = await render_template(
+        'shutdown.html',
+        mode=mode
+    )
+
+    if coro is not None:
+        loop = asyncio.get_running_loop()
+        loop.create_task(coro)
+
+    return rendered, 200 + 200 * (mode is None)
 
 
 if __name__ == '__main__':
