dff377631a68f88f09749d2fef6e74319d4d5aae
thegamecracks <61257169+thegamecracks@users.noreply.github.com>
2021-06-29 12:11:07 -0400

    Update test.py

--- bot/cogs/test.py
+++ bot/cogs/test.py
@@ -6,6 +6,50 @@ import discord
 from discord.ext import commands
 
 
+class ArrowButton(discord.ui.Button):
+    def __init__(self, direction, **kwargs):
+        super().__init__(
+            style=discord.ButtonStyle.primary,
+            **kwargs
+        )
+        self.direction = direction
+
+    async def callback(self, interaction):
+        link = 'https://discordpy.readthedocs.io/en/master/api.html#discord.InteractionResponse.send_message'
+        await interaction.response.send_message(f'You clicked [{self.direction}]({link})!', ephemeral=True)
+
+
+class ArrowView(discord.ui.View):
+    def __init__(self):
+        super().__init__(timeout=30)
+        self.message = None
+
+        self.add_empty(row=0)
+        self.add_arrow('up', label='🠕', row=0)
+        self.add_empty(row=0)
+        self.add_arrow('left', label='🠔', row=1)
+        self.add_arrow('down', label='🠗', row=1)
+        self.add_arrow('right', label='🠖', row=1)
+
+    def add_arrow(self, direction, **kwargs):
+        button = ArrowButton(direction, **kwargs)
+        self.add_item(button)
+        return button
+
+    def add_empty(self, row=None):
+        button = discord.ui.Button(
+            style=discord.ButtonStyle.secondary,
+            label='\u200b',
+            disabled=True,
+            row=row
+        )
+        self.add_item(button)
+        return button
+
+    async def on_timeout(self):
+        await self.message.edit(content='timeout!')
+
+
 class Testing(commands.Cog, command_attrs={'hidden': True}):
     """Testing commands."""
 
@@ -15,44 +59,26 @@ class Testing(commands.Cog, command_attrs={'hidden': True}):
     async def cog_check(self, ctx):
         return await ctx.bot.is_owner(ctx.author)
 
+    @commands.group()
+    async def foo(self, ctx):
+        print('foo', ctx.invoked_subcommand)
 
+    @foo.group()
+    async def bar(self, ctx):
+        print('  bar', ctx.invoked_subcommand)
 
+    @bar.command(name='2000')
+    async def _2000(self, ctx):
+        print('    2000', ctx.invoked_subcommand)
 
-
-    @commands.Cog.listener('on_socket_response')
-    async def on_button_interaction(self, m):
-        def map_buttons():
-            buttons = {}
-            for action_row in d['message']['components']:
-                for component in action_row['components']:
-                    custom_id = component.get('custom_id')
-                    if custom_id:
-                        buttons[custom_id] = component
-            return buttons
-
-        if m['t'] != 'INTERACTION_CREATE':
-            return
-
-        d = m['d']
-        data = d['data']
-
-        if 'component_type' not in data:
-            return
-
-        route = discord.http.Route(
-            'POST', '/interactions/{id}/{token}/callback',
-            id=d['id'], token=d['token']
-        )
-
-        # Get the button that was clicked
-        button = map_buttons()[data['custom_id']]
-
-        # send initial response
-        payload = {'type': 4, 'data': {
-            'content': f"Clicked {button['label']}",
-            'flags': 64  # makes message ephemeral/hidden
-        }}
-        await self.bot.http.request(route, json=payload)
+    @commands.command()
+    @commands.max_concurrency(1, commands.BucketType.default)
+    async def buttons(self, ctx):
+        """Buttons!"""
+        view = ArrowView()
+        message = await ctx.send('buttons!', view=view)
+        view.message = message
+        await view.wait()
 
 
 
