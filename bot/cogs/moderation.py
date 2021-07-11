045f68d492ad8629b3270ee0c645684aa50c0925
thegamecracks <61257169+thegamecracks@users.noreply.github.com>
2021-07-10 22:28:17 -0400

    Detect and delete outside invites for certain servers
    This only applies to my server and the Signal Hill server at the moment and is hardcoded since I don't plan on making it adjustable

--- bot/cogs/moderation.py
+++ bot/cogs/moderation.py
@@ -3,6 +3,8 @@
 #  License, v. 2.0. If a copy of the MPL was not distributed with this
 #  file, You can obtain one at http://mozilla.org/MPL/2.0/.
 import collections
+import re
+from typing import Optional
 
 import discord
 from discord.ext import commands
@@ -44,8 +46,166 @@ class PurgeLimitConverter(commands.Converter):
 class Moderation(commands.Cog):
     """Commands to be used in moderation."""
 
+    INVITE_REGEX = re.compile(r'(?:https?://)?discord(?:app)?\.(?:com/invite|gg)/(?P<code>[a-zA-Z0-9]+)/?')
+    SERVER_SETTINGS = {
+        # My Server
+        297463612870492160: {
+            'delete-invites': True,
+            'log-channel': 852021637452267591,
+            'whitelisted-roles': [322518663716536322]
+        },
+        # Signal Hill
+        811415496036843550: {
+            'delete-invites': True,
+            'log-channel': 811415497362505775,
+            'whitelisted-roles': [811415496075247649]
+        },
+    }
+
     def __init__(self, bot):
         self.bot = bot
+
+        self._invite_cache = {}  # str: Optional[discord.Invite]
+        # NOTE: this should be a temporary cache but I'm lazy
+
+
+
+
+
+    async def _invite_fetch_cached(self, m: re.Match) -> Optional[discord.Invite]:
+        try:
+            invite = self._invite_cache[m.group('code')]
+        except KeyError:
+            try:
+                invite = await self.bot.fetch_invite(m.group())
+            except discord.NotFound:
+                invite = None
+            finally:
+                self._invite_cache[m.group('code')] = invite
+        return invite
+
+
+    async def _invite_search_content(
+            self, guild_id: int, content: str) -> Optional[discord.Invite]:
+        # Find all invites in the message
+        matches = tuple(self.INVITE_REGEX.finditer(content))
+        if not matches:
+            return
+
+        # Return the first invite that doesn't link to the guild
+        for m in matches:
+            invite = await self._invite_fetch_cached(m)
+            if invite is None:
+                continue
+            elif not invite.guild or invite.guild.id != guild_id:
+                return invite
+
+
+    async def _invite_delete_and_log(
+            self,
+            message: discord.Message,
+            invite: discord.Invite,
+            *,
+            log_channel: Optional[discord.abc.Messageable],
+            is_edited: bool
+        ):
+        if not log_channel:
+            return await message.delete(delay=0)
+
+        embed = discord.Embed(
+            description='{sent} by: {mention}\nInvite: {url}'.format(
+                sent='Edited' if is_edited else 'Sent',
+                mention=message.author.mention,
+                url=invite.url
+            ),
+            timestamp=message.created_at
+        ).set_thumbnail(
+            url=message.author.avatar.url
+        ).set_footer(
+            text='Message sent'
+        )
+
+        try:
+            await message.delete()
+        except discord.HTTPException:
+            embed.title = 'Server Invite Detected'
+            embed.description += f'\n[Jump to message]({message.jump_url})'
+        else:
+            embed.title = 'Server Invite Deleted'
+
+        await log_channel.send(embed=embed)
+
+
+    @commands.Cog.listener('on_message')
+    async def on_invite_message(self, message: discord.Message):
+        """Delete and record messages that have server invites in them."""
+        # Ignore DMs and bots
+        if not message.guild or message.author.bot:
+            return
+
+        # Check if this server wants invites deleted
+        settings = self.SERVER_SETTINGS.get(message.guild.id)
+        if settings is None:
+            return
+        elif not settings['delete-invites']:
+            return
+
+        roles = settings['whitelisted-roles']
+        if roles and any(message.author._roles.get(n) for n in roles):
+            return
+
+        invite = await self._invite_search_content(message.guild.id, message.content)
+        if invite is None:
+            return
+
+        await self._invite_delete_and_log(
+            message, invite,
+            log_channel=message.guild.get_channel(settings['log-channel']),
+            is_edited=False
+        )
+
+
+    @commands.Cog.listener('on_raw_message_edit')
+    async def on_invite_message_edit(
+            self, payload: discord.RawMessageUpdateEvent):
+        if payload.guild_id is None:
+            return
+
+        settings = self.SERVER_SETTINGS.get(payload.guild_id)
+        if settings is None:
+            return
+        elif not settings['delete-invites']:
+            return
+
+        content = payload.data.get('content')
+        if content is None:
+            # Something else in the message was edited, e.g. embeds
+            return
+
+        invite = await self._invite_search_content(payload.guild_id, content)
+        if invite is None:
+            return
+
+        # Resolve into Message object
+        guild = self.bot.get_guild(payload.guild_id)
+        sender_channel = guild.get_channel(payload.channel_id)
+        message = await sender_channel.fetch_message(payload.message_id)
+
+        if message.author.bot:
+            return
+
+        roles = settings['whitelisted-roles']
+        if roles and any(message.author._roles.get(n) for n in roles):
+            return
+
+        await self._invite_delete_and_log(
+            message, invite,
+            log_channel=message.guild.get_channel(settings['log-channel']),
+            is_edited=True
+        )
+
+
+
 
 
     @commands.Cog.listener('on_message')
