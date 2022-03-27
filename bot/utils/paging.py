a50cb6daef3f9bd4951d8db2454bfa854e8e1c88
thegamecracks <61257169+thegamecracks@users.noreply.github.com>
2022-03-27 11:00:49 -0400

    Add paginator support for an AsyncIteratorPageSource
    - get_page(), get_page_options(), and format_page() can now be asynchronous methods
    - PaginatorView can now be disabled by page sources if `max_pages` returns 0
    - PaginatorView now stops itself if all of its features are not needed (pagination/navigation/going back)
    - Fixes potential TypeError when navigating

--- bot/utils/paging.py
+++ bot/utils/paging.py
@@ -3,7 +3,7 @@
 #  License, v. 2.0. If a copy of the MPL was not distributed with this
 #  file, You can obtain one at http://mozilla.org/MPL/2.0/.
 import functools
-from typing import TypeVar, Generic, Collection, TypedDict, Any, cast, Protocol
+from typing import TypeVar, Generic, Collection, TypedDict, Any, cast, Protocol, AsyncIterator
 
 import discord
 
@@ -19,8 +19,8 @@ class PageParams(TypedDict, total=False):
 
 
 class PageOption(discord.SelectOption, Generic[S]):
-    """A select option for a particular page which
-    allows nested pages through the added `source=` kwarg.
+    """A select option that can store a nested page
+    through the added `source=` kwarg.
     """
     def __init__(self, *args, source: S, **kwargs):
         super().__init__(*args, **kwargs)
@@ -28,28 +28,49 @@ class PageOption(discord.SelectOption, Generic[S]):
 
 
 class PageSourceProto(Protocol[T, S, V]):
-    def get_page(self, index: int) -> T: ...
+    def get_page(self, index: int) -> T:
+        """Returns a page based on the given index.
+
+        This method may be asynchronous.
+
+        """
 
     @property
-    def max_pages(self) -> int: return 0
+    def max_pages(self) -> int:
+        """The max number of pages the page source can return.
+
+        Can return zero to disable the view entirely.
+
+        """
+        return 1
 
     def get_page_options(self, view: V, page: T) -> list[PageOption[S]]:
-        """Returns a list of page options for the user to select."""
+        """Returns a list of page options for the user to select.
+
+        This method may be asynchronous.
+
+        """
         return []
 
     def format_page(self, view: V, page: T) -> PageParams | str | discord.Embed:
-        """Returns a dictionary presenting the items in the page."""
+        """Returns a dictionary presenting the items in the page.
+
+        This method may be asynchronous.
+
+        """
         raise NotImplementedError
 
 
 # noinspection PyAbstractClass
 class PageSource(PageSourceProto, Generic[T, S, V]):
+    """The base page source class."""
     def __init__(self, current_index: int = 0):
         self.current_index = current_index
 
 
 # noinspection PyAbstractClass
 class ListPageSource(PageSource[list[E], S, V]):
+    """Paginates a list of elements."""
     def __init__(self, items: list[E], *args, page_size: int, **kwargs):
         super().__init__(*args, **kwargs)
         self.items = items
@@ -65,8 +86,59 @@ class ListPageSource(PageSource[list[E], S, V]):
         return pages + bool(remainder)
 
 
+# noinspection PyAbstractClass
+class AsyncIteratorPageSource(PageSource[list[E], S, V]):
+    """Paginates an async iterator."""
+    def __init__(self, iterator: AsyncIterator[E], *args, page_size: int, **kwargs):
+        super().__init__()
+        self._cache: list[E] = []
+        self._iterator = iterator
+        self._max_index = 0
+        self._exhausted = False
+        self.page_size = page_size
+
+    async def get_page(self, index: int):
+        start = index * self.page_size
+        end = start + self.page_size
+        if self._exhausted:
+            return self._cache[start:end]
+
+        # Get enough items to have at least one extra page
+        # so paginator knows if next/last buttons should turn off
+        required = end + self.page_size - len(self._cache)
+        if required > 0:
+            new_items = []
+            max_index = index + 1
+            for i in range(required):
+                try:
+                    new_items.append(await anext(self._iterator))
+                except StopAsyncIteration:
+                    max_index = (len(self._cache) + i) // self.page_size
+                    self._exhausted = True
+                    break
+            self._cache.extend(new_items)
+            self._max_index = max_index
+
+        return self._cache[start:end]
+
+    @property
+    def max_pages(self):
+        if self._exhausted and not self._cache:
+            return 0
+        return self._max_index + 1
+
+
 class PaginatorView(discord.ui.View):
     """A view that handles pagination and recursive levels of pages.
+
+    To use this view, pass a PageSource or list of PageSources
+    (the last one will be displayed first) to the `sources=` kwarg,
+    then begin the paginator using `await view.start(channel)`.
+
+    If adding the view to another message is desired,
+    `view.message` must be manually set for it to be accessible.
+    The initial page is not rendered until the user interacts
+    with the paginator or is explicitly done with `await view.show_page()`.
 
     `interaction_check` by default will return True if `allowed_users`
     is None, or the interaction user is one of the allowed users.
@@ -98,11 +170,6 @@ class PaginatorView(discord.ui.View):
         self.page: PageParams = {}
         self.options: list[discord.SelectOption] = []
 
-        self.can_paginate = True
-        self.can_navigate = True
-
-        self.show_page(self.current_source.current_index)
-
     @property
     def current_source(self):
         return self.sources[-1]
@@ -115,15 +182,28 @@ class PaginatorView(discord.ui.View):
     def current_index(self, index: int):
         self.current_source.current_index = index
 
+    @property
+    def can_navigate(self):
+        return len(self.options) > 0
+
+    @property
+    def can_paginate(self):
+        return self.current_source.max_pages > 1
+
+    @property
+    def can_go_back(self):
+        return len(self.sources) > 1
+
     @functools.cached_property
     def _pagination_buttons(self) -> tuple[discord.ui.Button]:
         return self.first_page, self.prev_page, self.next_page, self.last_page  # type: ignore
 
-    def show_page(self, index: int):
+    async def show_page(self, index: int):
         self.current_index = index
+        maybe_coro = discord.utils.maybe_coroutine
 
-        page = self.current_source.get_page(index)
-        params = self.current_source.format_page(self, page)
+        page = await maybe_coro(self.current_source.get_page, index)
+        params = await maybe_coro(self.current_source.format_page, self, page)
         if isinstance(params, str):
             params = {'content': params}
         elif isinstance(params, discord.Embed):
@@ -132,12 +212,15 @@ class PaginatorView(discord.ui.View):
             raise TypeError('format_page() must return a dict, str, or Embed')
         self.page = cast(dict, params)
 
-        self.options = self.current_source.get_page_options(self, page)
-        for i, option in enumerate(self.options):
+        options = await maybe_coro(self.current_source.get_page_options, self, page)
+        for i, option in enumerate(options):
             option.value = str(i)
+        self.options = options
+
         self._refresh_components()
 
     async def start(self, channel: discord.abc.Messageable):
+        await self.show_page(self.current_source.current_index)
         self.message = await channel.send(**self._get_message_kwargs())
 
     async def interaction_check(self, interaction):
@@ -146,7 +229,12 @@ class PaginatorView(discord.ui.View):
         return True
 
     def _get_message_kwargs(self) -> dict[str, Any]:
-        return {'view': self, **self.page}
+        kwargs = {**self.page}
+        max_pages = self.current_source.max_pages
+        can_interact = self.can_navigate or self.can_paginate or self.can_go_back
+        if max_pages > 0 and can_interact:
+            kwargs['view'] = self
+        return kwargs
 
     async def _respond(self, interaction: discord.Interaction):
         await interaction.response.edit_message(**self._get_message_kwargs())
@@ -172,66 +260,66 @@ class PaginatorView(discord.ui.View):
 
         """
         self.clear_items()
-        can_navigate = len(self.options) > 0
-        can_paginate = self.current_source.max_pages > 1
 
         # Navigation (select menu)
-        if can_navigate:
+        if self.can_navigate:
             self.add_item(self.navigate)
             self.navigate.options = self.options
 
         # Pagination (left/right)
-        if can_paginate:
+        if self.can_paginate:
             for button in self._pagination_buttons:
                 self.add_item(button)
-                self.can_paginate = True
 
         on_first_page = self.current_index == 0
-        on_last_page = self.current_index + 1 == self.current_source.max_pages
+        on_last_page = self.current_index + 1 >= self.current_source.max_pages
         self.first_page.disabled = on_first_page
         self.prev_page.disabled = on_first_page
         self.next_page.disabled = on_last_page
         self.last_page.disabled = on_last_page
 
         # Back and stop buttons
-        if len(self.sources) > 1:
-            self.back_button.row = 1 + can_paginate
+        if self.can_go_back:
+            self.back_button.row = 1 + self.can_paginate
             self.add_item(self.back_button)
         self.add_item(self.stop_button)
 
     @discord.ui.select(options=[], placeholder='Navigate...', row=0)
     async def navigate(self, interaction, select: discord.ui.Select):
-        option = cast(PageOption, discord.utils.get(select.options, value=select.values[0]))
+        option = cast(
+            PageOption,
+            discord.utils.get(self.navigate.options, value=select.values[0])
+        )
         self.sources.append(option.source)
-        self.show_page(self.current_index)
+        await self.show_page(self.current_index)
         await self._respond(interaction)
 
     @discord.ui.button(
         emoji='\N{BLACK LEFT-POINTING DOUBLE TRIANGLE WITH VERTICAL BAR}',
         style=discord.ButtonStyle.blurple, row=1)
     async def first_page(self, interaction, button):
-        self.show_page(0)
+        await self.show_page(0)
         await self._respond(interaction)
 
     @discord.ui.button(
         emoji='\N{BLACK LEFT-POINTING TRIANGLE}',
         style=discord.ButtonStyle.blurple, row=1)
     async def prev_page(self, interaction, button):
-        self.show_page(self.current_index - 1)
+        await self.show_page(self.current_index - 1)
         await self._respond(interaction)
 
     @discord.ui.button(
         emoji='\N{BLACK RIGHT-POINTING TRIANGLE}',
         style=discord.ButtonStyle.blurple, row=1)
     async def next_page(self, interaction, button):
-        self.show_page(self.current_index + 1)
+        await self.show_page(self.current_index + 1)
         await self._respond(interaction)
 
     @discord.ui.button(
         emoji='\N{BLACK RIGHT-POINTING DOUBLE TRIANGLE WITH VERTICAL BAR}',
         style=discord.ButtonStyle.blurple, row=1)
     async def last_page(self, interaction, button):
-        self.show_page(self.current_source.max_pages - 1)
+        await self.show_page(self.current_source.max_pages - 1)
         await self._respond(interaction)
 
     @discord.ui.button(
@@ -246,5 +334,5 @@ class PaginatorView(discord.ui.View):
         style=discord.ButtonStyle.blurple, row=2)
     async def back_button(self, interaction, button):
         self.sources.pop()
-        self.show_page(self.current_index)
+        await self.show_page(self.current_index)
         await self._respond(interaction)
