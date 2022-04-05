85b49be2adc99d58d875d4657e5ae553e710899f
thegamecracks <61257169+thegamecracks@users.noreply.github.com>
2022-04-04 22:27:37 -0400

    Log warnings and above to console

--- main.py
+++ main.py
@@ -40,11 +40,22 @@ EXT_LIST.append('jishaku')
 
 logger = logging.getLogger('discord')
 logger.setLevel(logging.INFO)
-handler = logging.FileHandler(
-    filename='discord.log', encoding='utf-8', mode='w')
-handler.setFormatter(logging.Formatter(
-    '%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
-logger.addHandler(handler)
+
+file_handler = logging.FileHandler(
+    filename='discord.log', encoding='utf-8', mode='w'
+)
+file_handler.setFormatter(
+    logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s')
+)
+logger.addHandler(file_handler)
+
+stream_handler = logging.StreamHandler()
+stream_handler.setLevel(logging.WARNING)
+stream_handler.setFormatter(
+    logging.Formatter('%(levelname)s in %(name)s: %(message)s')
+)
+logger.addHandler(stream_handler)
+# FIXME: logs get duplicated in console for some reason
 
 
 class UserWithTimezone(typing.TypedDict):
