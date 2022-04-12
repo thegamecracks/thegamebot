8db71971b1e6997788bc103ded1dd9e547821aae
thegamecracks <61257169+thegamecracks@users.noreply.github.com>
2022-04-12 07:34:41 -0400

    Remove announcement-type reminders
    This implementation was kind of hacky since it used the content of the message itself to distinguish announcement-type reminders. A preferable solution would be to have a specific announcement command and a boolean column in the database instead, that way they become independent of content and can have separate checks/cooldowns from regular reminders.

--- bot/cogs/reminders.py
+++ bot/cogs/reminders.py
@@ -2,7 +2,6 @@
 #  This Source Code Form is subject to the terms of the Mozilla Public
 #  License, v. 2.0. If a copy of the MPL was not distributed with this
 #  file, You can obtain one at http://mozilla.org/MPL/2.0/.
-# TODO: get rid of the announcement feature until it's properly implemented
 import asyncio
 import datetime
 import functools
@@ -110,24 +109,12 @@ async def query_reminder_count(conn: asqlite.Connection, user_id: int) -> int:
         return row['length']
 
 
-OptionalCurrentChannel = commands.parameter(
-    converter=discord.TextChannel | None,
-    default=lambda ctx: ctx.channel,
-    displayed_default='<this channel>'
-)
-
-
 class Reminders(commands.Cog):
     """Commands for setting up reminders."""
     qualified_name = 'Reminders'
 
-    ESCAPED_ROLE_MENTION = re.compile(r'\\<@&(\d+)>')
-    EVERYONE_ROLE_USER_MENTION = re.compile(r'@everyone|@here|<@[!&]?\d+>')
-    EVERYONE_ROLE_MENTION = re.compile(r'@everyone|@here|<@&(?P<id>\d+)>')
-
     MAXIMUM_REMINDERS = 10
     MAXIMUM_REMINDER_CONTENT = 250
-    MAXIMUM_ANNOUNCEMENT_CONTENT = 1000
 
     NEAR_DUE = datetime.timedelta(minutes=11)
     # NOTE: should be just a bit longer than task loop
@@ -156,16 +143,6 @@ class Reminders(commands.Cog):
         entry['reminder_id'] = reminder_id
 
         return self.check_reminder(entry)
-
-    def is_announcement(self, content: str):
-        """Determine if a reminder's content is an announcement
-        (whether a user/role mention exists on the first line of the message).
-
-        When adding reminders in DMs, a newline can be prepended to the content
-        to prevent the reminder from being interpreted as an announcement.
-
-        """
-        return bool(self.EVERYONE_ROLE_USER_MENTION.search(content.split('\n', 1)[0]))
 
     @commands.group(aliases=('reminder',), invoke_without_command=True)
     @commands.cooldown(2, 6, commands.BucketType.user)
@@ -212,12 +189,7 @@ index: If provided, shows more details about the given reminder."""
 
             due = row['due'].replace(tzinfo=datetime.timezone.utc)
             embed = discord.Embed(
-                title='{} #{:,}'.format(
-                    'Announcement'
-                    if self.is_announcement(row['content'])
-                    else 'Reminder',
-                    index
-                ),
+                title=f'Reminder #{index:,d}',
                 description=row['content'],
                 color=ctx.bot.get_user_color(ctx.author)
             ).add_field(
@@ -277,38 +249,20 @@ Examples:
 
 
     @reminders.command(name='add')
-    async def reminders_add(
-        self, ctx: Context,
-        channel: discord.TextChannel = OptionalCurrentChannel,
-        *, time_and_reminder: Reminder
-    ):
+    async def reminders_add(self, ctx: Context, *, time_and_reminder: Reminder):
         """Create a reminder in the given or current channel.
 
 Usage:
     remind at 10pm EST to <x>
     remind in 30 sec/min/h/days to <x>
-    remind #announcements on wednesday to <x>
+    remind #bot-commands on wednesday to <x>
 Note that the time and your reminder message have to be separated with "to".
-If you have not explicitly said a timezone in the command but you have
-provided the bot your timezone before with "timezone set", that timezone will
+If you have not explicitly included a timezone in the command, but you have
+provided the bot your timezone before with "/timezone set", that timezone will
 be used instead of UTC.
 
-The channel parameter only allows channels in the same server where you can both send messages and mention everyone in.
-
-If you are able to mention everyone, this command can be used for scheduled announcements by following these steps:
-1. mention a member or role in the first line of your message, which hides the reminder header
-2. to avoid pinging with @\u200beveryone and @\u200bhere, use @all and @now instead
-3. as well, prefix role mentions with a backslash
 User mentions will otherwise be escaped except in DMs."""
-        # Validate channel and permissions
-        if channel.guild != ctx.guild:
-            return await ctx.message.add_reaction('\N{CROSS MARK}')
-
-        user_perms = channel.permissions_for(ctx.author)
-        if not user_perms.send_messages or not user_perms.mention_everyone:
-            return await ctx.message.add_reaction('\N{CROSS MARK}')
-
-        me_perms = channel.permissions_for(ctx.me)
+        me_perms = ctx.channel.permissions_for(ctx.me)
         if not me_perms.send_messages:
             return await ctx.message.add_reaction('\N{FACE WITHOUT MOUTH}')
 
@@ -324,34 +278,8 @@ User mentions will otherwise be escaped except in DMs."""
 
         due, content = time_and_reminder.due, time_and_reminder.content
 
-        if user_perms.mention_everyone:
-            # Parse message mentions for announcement support
-            max_content_size = self.MAXIMUM_ANNOUNCEMENT_CONTENT
-            content = content.replace('@all', '@everyone').replace('@now', '@here')
-            content = self.ESCAPED_ROLE_MENTION.sub(r'<@&\1>', content)
-
-            if isinstance(channel, discord.DMChannel):
-                # Prepend a newline so the message is not
-                # considered an announcement (yes, this is stupid)
-                content = '\n' + content
-            elif not me_perms.mention_everyone:
-                # Prevent creating the announcement if there's a mention
-                # the bot cannot do properly
-                for m in self.EVERYONE_ROLE_MENTION.finditer(content):
-                    if m.group() in ('@everyone', '@here'):
-                        return await ctx.send(
-                            f'I am missing permissions to '
-                            f'ping everyone in {channel.mention}.'
-                        )
-                    role = channel.guild.get_role(int(m.group('id')))
-                    if role is not None and not role.mentionable:
-                        return await ctx.send(
-                            f'I am missing permissions to '
-                            f'properly ping the {role.mention} role.'
-                        )
-        else:
-            max_content_size = self.MAXIMUM_REMINDER_CONTENT
-            content = await commands.clean_content().convert(ctx, content)
+        max_content_size = self.MAXIMUM_REMINDER_CONTENT
+        content = await commands.clean_content().convert(ctx, content)
 
         diff = len(content) - max_content_size
         if diff > 0:
@@ -365,17 +293,15 @@ User mentions will otherwise be escaped except in DMs."""
 
         await self.add_reminder({
             'user_id': ctx.author.id,
-            'channel_id': channel.id,
+            'channel_id': ctx.channel.id,
             'due': due,
             'content': content
         })
 
         await ctx.send(
-            'Added your {} {} for {} in {}!'.format(
+            'Added your {} reminder for {} in this channel!'.format(
                 ctx.bot.inflector.ordinal(count + 1),
-                'announcement' if self.is_announcement(content) else 'reminder',
-                discord.utils.format_dt(due, style='F'),
-                'this channel' if channel == ctx.channel else channel.mention
+                discord.utils.format_dt(due, style='F')
             )
         )
 
@@ -440,16 +366,12 @@ This requires the Manage Messages permission in the given channel."""
 
 
     @commands.command(
-        name='remind', aliases=('remindme', 'announce'),
+        name='remind', aliases=('remindme',),
         brief='Shorthand for reminder add.',
         help=reminders_add.callback.__doc__
     )
-    async def remind(
-        self, ctx: Context,
-        channel: discord.TextChannel = OptionalCurrentChannel,
-        *, time_and_reminder: Reminder
-    ):
-        await self.reminders_add(ctx, channel, time_and_reminder=time_and_reminder)
+    async def remind(self, ctx: Context, *, time_and_reminder: Reminder):
+        await self.reminders_add(ctx, time_and_reminder=time_and_reminder)
 
     def check_reminder(self, entry: ReminderEntry, *, now=None):
         """Starts an :class:`asyncio.Task` to handle sending a reminder
@@ -542,23 +464,16 @@ This requires the Manage Messages permission in the given channel."""
         # NOTE: multiple API calls could be made above if
         # member/user left several reminders
 
-        description = []
-        if not self.is_announcement(entry['content']):
-            # Add a header to show some extra info about the reminder
-            description.append(
-                '<@{}> **{}'.format(
-                    entry['user_id'],
-                    discord.utils.format_dt(entry['due'], style='F')
-                )
-            )
-            if seconds < 0:
-                description.append(' (overdue)')
-            description.append('**\n')
-        description.append(entry['content'])
+        description = '<@{mention}> **{due}{is_overdue}**\n{content}'.format(
+            mention=entry['user_id'],
+            due=discord.utils.format_dt(entry['due'], style='F'),
+            is_overdue=' (overdue)' * (seconds < 0),
+            content=entry['content']
+        )
 
         # Now we can try to send the reminder
         try:
-            await channel.send(''.join(description))
+            await channel.send(description)
         except discord.Forbidden as e:
             await remove_entry(
                 f'Reminders: failed to send reminder {reminder_id}: '
